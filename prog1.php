8bcb224a2abdd396cf0375775e92dc8189da65ce
noam3127 <noshev27@gmail.com>
2013-08-02 14:24:52 -0400

    simple prog.

---
+++ prog1.php
@@ -0,0 +1,25 @@
+<?php
+
+class blueprint {
+  
+  private $myname;
+  public $email;
+  public $address;  
+ 
+  function setmyname($val) {
+    $this->myname = ($val);
+  }
+  function getmyname() {
+      return $this->myname;
+ }
+}
+class radio extends blueprint {
+  public $station = '105.9';
+}
+$object = new radio;
+$object->email = 'noam@gmail.com';
+$object->address = '1600 Pennsylvania Ave.';
+$object->setmyname('Noam');
+print_r($object);
+
+?>
